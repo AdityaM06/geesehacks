316e138c00b3557f430f1eaf0400052f5f34c539
KurtisL7 <kurtislin24@gmail.com>
2025-01-25 15:44:50 -0500

    working on the frontpage

--- geesehacks/src/App.js
+++ geesehacks/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // Ben naming convetion
+    <div className="app">
+      <h1>Hey everyone</h1>
+
+      <div className="app_top">
+      {/* Logo of the geesehacks replace after finish */} 
+      <img
+      className="app_logo" src="https://media.istockphoto.com/id/1421301993/photo/shopping-cart-sign-on-the-cube-shape-dice.jpg?s=1024x1024&w=is&k=20&c=r8uASh30J26EJ0O9LKuA4HCbe5_r6ZXdTehJXSSLwJQ="
+      alt =" "
+      />
+      <h1>Reels</h1>
+      </div>
+
+
+      <div className="app_video">
+      {/*video apps on the screen*/}
+      
+      {/* video */}
+      {/* video */}
+      {/* video */}
+      {/* video */}
+      </div>
     </div>
   );
 }
