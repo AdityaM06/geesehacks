ee41be5d21f11e0b4ab8879163e8053d11017065
Aditya Manuja <adimanuja@gmail.com>
2025-01-26 05:09:03 -0500

    Bootstrap

--- geesehacks/src/components/footer.tsx
+++ geesehacks/src/components/footer.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './footer.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer-container">
       <Link to="/" className="footer-link">
-        <img src="/path/to/home-icon.png" alt="Home" className="footer-icon" />
+        <i className="bi bi-house-door footer-icon" aria-hidden="true"></i>
       </Link>
       <Link to="/video" className="footer-link">
-        <img src="/path/to/video-icon.png" alt="Video" className="footer-icon" />
+        <i className="bi bi-file-play footer-icon" aria-hidden="true"></i>
       </Link>
     </footer>
   );
