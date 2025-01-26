a4a10119800333c38581ef3e01ae11fde2629fac
KurtisL7 <kurtislin24@gmail.com>
2025-01-25 19:11:26 -0500

    replacing and fixing

--- geesehacks/src/App.js
+++ geesehacks/src/App.js
@@ -1,5 +1,6 @@
-
 import './App.css';
+import VideoCard from "./VideoCard";
+import VideoCard2 from "./VideoCard2";
 
 function App() {
   return (
@@ -11,8 +12,10 @@ function App() {
       <div className="app_top">
       {/* Logo of the geesehacks replace after finish */} 
       <img
-      className="app_logo" src="https://media.istockphoto.com/id/1421301993/photo/shopping-cart-sign-on-the-cube-shape-dice.jpg?s=1024x1024&w=is&k=20&c=r8uASh30J26EJ0O9LKuA4HCbe5_r6ZXdTehJXSSLwJQ="
-      alt =" "
+      className="app_logo" 
+      alt=''
+      src="https://media.istockphoto.com/id/1421301993/photo/shopping-cart-sign-on-the-cube-shape-dice.jpg?s=1024x1024&w=is&k=20&c=r8uASh30J26EJ0O9LKuA4HCbe5_r6ZXdTehJXSSLwJQ="
+      
       />
       <h1>Reels</h1>
       </div>
@@ -20,7 +23,10 @@ function App() {
 
       <div className="app_video">
       {/*video apps on the screen*/}
-      
+      <VideoCard/>
+      <VideoCard2 />
+      <VideoCard />
+      <VideoCard />
       {/* video */}
       {/* video */}
       {/* video */}
